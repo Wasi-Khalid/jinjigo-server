44d97240a277b2d0726e1e1e2b4b499a1453dbdc
Wasi_Khalid <aman.wasi01@gmail.com>
2024-05-21 00:00:43 +0500

    bug fixed

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables
 require('./jwt/generateKey');
 const express = require('express');
+const cors = require('cors');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const cookieSession = require('cookie-session');
-const setupMiddlewares = require('./middlewares/middleware');
 const authRoutes = require('./routes/authRoutes');
 const calendarRoutes = require('./routes/calendarRoutes');
 const db = require('./database/db');
@@ -15,13 +15,12 @@ db.connect();
 
 const app = express();
 
-// Set up passport to use Google OAuth
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.NODE_ENV === 'production'
         ? 'https://jinjigo-server.onrender.com/auth/google/callback'
-        : 'http://localhost:3000/auth/google/callback',
+        : 'http://localhost:5174/auth/google/callback',
 }, async (token, tokenSecret, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
@@ -60,8 +59,14 @@ passport.deserializeUser(async (id, done) => {
     }
 });
 
-// Use the middleware configuration
-setupMiddlewares(app);
+app.use(cors({ origin: process.env.CORS_ORIGIN, optionsSuccessStatus: 200 }));
+app.use(express.json());
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2'],
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
     res.send('Express Server');
@@ -76,7 +81,7 @@ app.get('/auth/google',
 app.get('/auth/google/callback',
     passport.authenticate('google', { failureRedirect: '/' }),
     (req, res) => {
-        res.redirect('https://jinjigo.vercel.app'); // Redirect to your React app
+        res.redirect('https://jinjigo.vercel.app');
     });
 
 app.get('/logout', (req, res) => {
